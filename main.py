d1877ffd67321003c769ea63f17dfb9bac4d0dc1
JaywenTan <160487275+JaywenTan@users.noreply.github.com>
2024-03-10 11:46:29 +0000

    Final Submission

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ stdform = input('Enter a number in scientific notation: ')
 stdform = stdform.strip()
 
 # Type your code below
+a = stdform.find("x")
+b = stdform.find("^")
+text = stdform[:a] + "E" + stdform[b+1:]
+print("This number in E notation is {}.".format(text))
+
+
